89de8b340e150cdd18208af1769eae71e76de654
madisonjames <madison.james1@my.jcu.edu>
2018-09-13 15:52:14 +1000

    Add more changes to prac 6

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,7 +1,7 @@
 from prac_06.guitar import Guitar
 
-def main():
 
+def main():
     guitar_list = []
     print("My guitars!")
     name = input("Name:")
@@ -15,17 +15,13 @@ def main():
     guitar_list.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitar_list.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
     print("These are my guitars:")
-    for i, guitar in enumerate(guitar_list):
-        if Guitar.is_vintage(guitar):
-            vintage_string = "(vintage)"
-            print("Guitar {}: {:>20} ({}), worth ${:1,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
-                                                                  vintage_string))
+    for i, guitar in enumerate(guitar_list, 1):
+        if guitar.is_vintage():
+            vintage_string = " (vintage)"
         else:
             vintage_string = " "
-            print("Guitar {}: {:>20} ({}), worth ${:1,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
-                                                                      vintage_string))
-
-
+        print("Guitar {}: {:>20} ({}), worth ${:1,.2f}{}".format(i, guitar.name, guitar.year, guitar.cost,
+                                                                 vintage_string))
 
 
 main()
